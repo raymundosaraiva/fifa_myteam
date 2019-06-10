349b8e78acd9677e77d81259415c2f4b0e606c3e
Ray <raymundosaraiva@hotmail.com>
2019-06-09 22:45:57 -0300

    Fix order

--- myteam/views.py
+++ myteam/views.py
@@ -20,7 +20,7 @@ GK_NUM = 1
 
 # @cache_page(60 * 60 * 24 * 10, cache='default', key_prefix='')
 def index(request):
-    players = Player.objects.all()
+    players = Player.objects.all().order_by('-Overall')
     nat = players.values('Nationality', 'Flag').distinct()[:10]
     club = players.values('Club', 'ClubLogo').distinct()[:10]
     context = {
@@ -57,12 +57,12 @@ def get_team(request):
         'Age__gte': age,
     }
 
-    player = Player.objects.filter(**player_filter)
+    player = Player.objects.filter(**player_filter).order_by('-Overall')
 
     f = player.filter(Position__regex=ATT)[:ATT_NUM]
     m = player.filter(Position__regex=MID)[:MID_NUM]
     b = player.filter(Position__regex=DEF)[:DEF_NUM]
-    gk = Player.objects.filter(**gk_filter, Position__regex=GK)[:GK_NUM]
+    gk = Player.objects.filter(**gk_filter, Position__regex=GK).order_by('-Overall')[:GK_NUM]
 
     status = False
     if f.count() >= ATT_NUM and m.count() >= MID_NUM and b.count() >= DEF_NUM and gk.count() >= GK_NUM:
